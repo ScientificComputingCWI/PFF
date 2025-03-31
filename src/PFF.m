159dcd7c3b0dde2b5871a245a76bb9c369e05f23
Benjamin Sanderse <bsanderse@gmail.com>
2025-03-31 21:12:37 +0200

    added comments and removed old code

--- src/PFF.m
+++ src/PFF.m
@@ -1,7 +1,9 @@
-function residual = PFF(t,x,param) %d,q,p)
+function residual = PFF(~,x,param)
 
 % state x: matrix of Np particles (rows) each of dimension N, so Np*N matrix
 % implementation so far tested for N=1
+
+Np = size(x,1); % number of particles
 
 d = param.d;
 q = param.q;
@@ -9,30 +11,16 @@ p = param.p;
 
 % d: observation
 % kernel matrix A
-% observation operator H, so y = H(x) and derivative J = dHdx
+% observation operator H, in this case simply y(x) and derivative J = dH/dx
+% = dy/dx
 % likelihood weight R
 % parameters p
 
 % see also Algorithm 1 in Hu & van Leeuwen
 
-Np = size(x,1); %p_PFF.Np; % number of particles
-
 
 % initialize residual
 res = zeros(Np,1);
-
-% this is the simplified form where a linear observation operator has been
-% substituted
-% for i=1:Np
-% 
-%     for j=1:Np
-% 
-%         res(i) = res(i) + RKHS(x(j,:),x(i,:),p) * ...
-%                  ( -(A'*(x(j,:) - x(i,:))) + H'*R\(d-H*x(j,:)) );
-% 
-%     end
-% 
-% end
 
 
 A  = p.PFF.A;
@@ -45,12 +33,16 @@ grad_log_p = zeros(Np,1);
 % precompute the log posterior term, so that the model is only evaluated Np
 % times instead of Np^2 times
 for i=1:Np
-    % J = p.model.beta;
-    % J = (1+3*p.model.beta*x(i,:).^2);
-     % J = pi*cos(pi*x(i,:));
+
+    % evaluate model and get its Jacobian
     [y,J] = model(x(i),q(i),p);
+    
+    % evaluate gradient of log likelihood
     [~,dlogp_dx] = likelihood(y,d,p);
+
     % note that if J is a scalar then J.'=J
+    % evaluate entire gradient of log posterior =  grad. log likelihood  +
+    % grad log prior
     grad_log_p(i) = J*dlogp_dx - B\(x(i,:) - xb); 
 
 end
@@ -61,20 +53,23 @@ for i=1:Np
     kernel  = RKHS(x(:,:),x(i,:),p.PFF);
     res(i)  = sum(kernel .* ( -(A.'*(x(:,:) - x(i,:)))+ grad_log_p ));
 
-    % for j=1:Np
-    % 
-    %     % res(i) = res(i) + RKHS(x(j,:),x(i,:),p) * ...
-    %     %          ( -(A'*(x(j,:) - x(i,:))) + H'*R\(d-H*x(j,:)) );
-    % 
-    %     res(i) = res(i) + RKHS(x(j,:),x(i,:),p.PFF) * ...
-    %              ( -(A.'*(x(j,:) - x(i,:))) + ... % div K
-    %               grad_log_p(j) );  % grad log post
-    % 
-    % 
-    % end
 end
 
+% non-vectorized form
+% for i=1:Np
+% 
+% 
+%     for j=1:Np
+% 
+%         res(i) = res(i) + RKHS(x(j,:),x(i,:),p.PFF) * ...
+%                  ( -(A.'*(x(j,:) - x(i,:))) + ... % div K
+%                   grad_log_p(j) );  % grad log post
+% 
+% 
+%     end
+% end
 
+% scale with number of particles and scale
 % residual = B*res/Np;
 residual = res/Np;
     
